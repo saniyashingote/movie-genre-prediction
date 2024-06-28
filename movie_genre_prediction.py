943d96c0f21fa487f67ba83190c195c66431eaa5
saniyashingote <saniyashingote@gmail.com>
2024-06-28 18:37:41 +0530

    Update movie_genre_prediction.py

--- movie_genre_prediction.py
+++ movie_genre_prediction.py
@@ -26,6 +26,8 @@ df = pd.read_csv('/content/wiki_movie_plots_deduped.csv')
 
 print(df.head())
 
+"""**Preprocess the data**"""
+
 df['Genre'] = df['Genre'].apply(lambda x: x.split(',')[0] if pd.notnull(x) else 'Unknown')
 
 df = df[df['Genre'] != 'Unknown']
@@ -42,6 +44,8 @@ df['processed_plot'] = df['Plot'].apply(preprocess_text)
 vectorizer = TfidfVectorizer(max_features=1000)
 X = vectorizer.fit_transform(df['processed_plot']).toarray()
 
+"""**Feature extraction**"""
+
 from sklearn.naive_bayes import MultinomialNB
 
 model = MultinomialNB()
@@ -51,14 +55,20 @@ df_sample = df.sample(frac=0.1, random_state=42)
 X = vectorizer.fit_transform(df_sample['processed_plot']).toarray()
 y = df_sample['Genre']
 
+"""**Train the model**"""
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = LogisticRegression(max_iter=200, solver='saga')
 model.fit(X_train, y_train)
 
+"""**Evaluate the model**"""
+
 y_pred = model.predict(X_test)
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 print(classification_report(y_test, y_pred))
+
+"""**Predict the genre**"""
 
 def predict_genre(plot):
     processed_plot = preprocess_text(plot)
